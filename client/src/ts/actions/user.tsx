804a4c37e175dc2b40c041c0f2479225feb5ee29
Tsubasa Ozawa <t.ozawa.sake@gmail.com>
2020-05-18 02:54:23 +0900

    styled-componentsの導入

--- client/src/ts/actions/user.tsx
+++ client/src/ts/actions/user.tsx
@@ -1,16 +1,16 @@
 import { useBindAction } from '../usehelpers/useBindActions';
-import { RightValue } from '../configs/right';
+// import { RightValue } from '../configs/right';
 
 export const SET_USER = 'USER_SET_USER' as const;
 
 type UserInfo = {
   id: number;
   userName: string;
-  role: {
-    roleAccessRights: {
-      accessRightId: RightValue;
-    }[];
-  };
+  // role: {
+  //   roleAccessRights: {
+  //     accessRightId: RightValue;
+  //   }[];
+  // };
 };
 
 export const setUser = (userInfo: UserInfo) => ({ type: SET_USER, userInfo });
--- client/src/ts/components/pages/LoginPage.tsx
+++ client/src/ts/components/pages/LoginPage.tsx
@@ -1,6 +1,24 @@
 import React, { FC, useEffect } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import useAsyncPostLogin from '../../usehelpers/async/useAsyncPostLogin';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import urls from '../../configs/urls';
+import { Formik, Form, Field } from 'formik';
+
+const GlobalStyle = createGlobalStyle`
+`;
+
+const Input = styled.input`
+  max-width: 100%;
+  width: 100%;
+  padding: 10px 5px;
+  font-size: 1.5rem;
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+`;
 
 type Values = {
   loginName: string;
@@ -11,19 +29,48 @@ const initialValues: Values = { loginName: '', loginPassword: '' };
 
 const LoginPage: FC = () => {
   const { onSubmit, errorMessage } = useAsyncPostLogin<Values, any>();
-
+  const history = useHistory();
+  useEffect(() => {
+    if (localStorage.getItem('userId')) {
+      history.push(urls.home.path);
+    }
+  }, []);
   return (
     <div>
+      <GlobalStyle />
       <section>
         <div>
           <div>
             <div>
-              <h3></h3>
+              <h3>Login</h3>
               <hr />
               <div>
                 <figure>
-                  <img></img>
+                  <img />
                 </figure>
+                <Formik initialValues={initialValues} onSubmit={onSubmit}>
+                  <Form>
+                    <div>
+                      <Field
+                        as={Input}
+                        name="loginName"
+                        placeholder="Your Name"
+                      />
+                    </div>
+                    <div>
+                      <Field
+                        as={Input}
+                        name="loginPassword"
+                        type="password"
+                        placeholder="Your Password"
+                      />
+                    </div>
+                    <button type="submit">
+                      Login<i aria-hidden="true"></i>
+                    </button>
+                  </Form>
+                </Formik>
+                <ErrorMessage>{errorMessage || null}</ErrorMessage>
               </div>
             </div>
           </div>
